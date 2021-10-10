b008c5955387e04a6705bb78a5f1fc43c50470ff
ossentoo <ossentoo@gmail.com>
2021-10-10 22:28:49 +0100

    refactored

--- src/webapp1/Program.cs
+++ src/webapp1/Program.cs
@@ -3,22 +3,31 @@ using ClassLib2;
 using ClassLib3;
 using ClassLib4;
 using ClassLib5;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace webapp1
 {
-    public class Program{
-        public static void Main(string[] args){
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
             var lib1 = new Class1();
             var lib2 = new Class2();
             var lib3 = new Class3();
             var lib4 = new Class4();
             var lib5 = new Class5();
-
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-        
-            app.Run();
+            
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
+
